b8d70b00e7cd0152cf255834eedbb04665816e95
Vinicius Castro <vinicius1595@gmail.com>
2018-12-10 16:52:49 -0200

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import Header from './components/Header';
+import Main from './pages/main';
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+);
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import './style.css';
+
+const Header = () => <header id="main-header">JSHUNT</header> 
+
+export default Header;
