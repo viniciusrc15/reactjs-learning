e8c1beb027555d90e3843ed131d183e5216a9395
Vinicius Castro <vinicius1595@gmail.com>
2018-12-12 17:26:07 -0200

    finish project learnig

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
 import Header from './components/Header';
-import Main from './pages/main';
+// import Main from './pages/main';
+import Routes from './router';
 
 const App = () => (
   <div>
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import api from '../../services/api';
 import './style.css'
 
@@ -35,19 +36,19 @@ export default class Main extends Component {
     }
 
     render() {
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
         return <div className="product-list">
             {products.map(product => (
                 <article key={product._id}>
                     <strong>{product.title}</strong>
                     <p>{product.description}</p>
 
-                    <a href="das">Detalhes</a>
+                    <Link to={`/product/${product._id}`}>Detalhes</Link>
                 </article>
             ))}
             <div className="actions">
-                <button onClick={this.prevPage}>Anterior</button>
-                <button onClick={this.nextPage}>Próximo</button>
+                <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
             </div>
         </div>
     }
